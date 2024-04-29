25369882125dc13621a92dc78660751f26c7cddc
AmiraxHamza <148680603+AmiraxHamza@users.noreply.github.com>
2024-04-29 10:22:41 +0100

    1st Add

---
+++ script.js
@@ -0,0 +1,24 @@
+const text = document.getElementById("text-input");
+const button = document.getElementById("check-btn");
+const result = document.getElementById("result");
+
+const alertMsg = () => {
+  if (text.value.length < 1) {
+    alert("Please input a value");
+  } else {
+    palindrome();
+  }
+};
+
+const palindrome = () => {
+  const runTest = text.value.toLowerCase().match(/[a-z0-9]/g);
+  const cleanText = runTest.join("");
+  const reverseText = runTest.reverse().join("");
+  if (cleanText === reverseText) {
+    result.innerHTML = `${text.value} is a palindrome`;
+  } else {
+    result.innerHTML = `${text.value} is not a palindrome`;
+  }
+};
+
+button.addEventListener("click", alertMsg);
